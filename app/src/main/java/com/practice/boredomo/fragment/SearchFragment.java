9a18954dbefaf3f571868313d45cebba37b41625
aaronalba <aaalba1@up.edu.ph>
2021-05-16 18:47:01 +0800

    add value change listener to the slider
    The Participant Slider has now an onValueChange listener which updates
    the Participant Value's text when the slider is changed.

--- app/src/main/java/com/practice/boredomo/fragment/SearchFragment.java
+++ app/src/main/java/com/practice/boredomo/fragment/SearchFragment.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.slider.Slider;
 import com.practice.boredomo.R;
 
 
@@ -17,10 +20,28 @@ import com.practice.boredomo.R;
  * @author Aaron Alba
  */
 public class SearchFragment extends Fragment {
+    private TextView mParticipantValue;
+    private Slider mParticipantSlider;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+        // set listener to the slider
+        mParticipantSlider = view.findViewById(R.id.search_participant_slider);
+        mParticipantValue = view.findViewById(R.id.search_participant_value);
+        mParticipantSlider.addOnChangeListener(new Slider.OnChangeListener() {
+            @Override
+            public void onValueChange(@NonNull Slider slider, float value, boolean fromUser) {
+                String text = ((int)value)+"";
+                mParticipantValue.setText(text);
+            }
+        });
+
+
+
+
         return view;
     }
 }
