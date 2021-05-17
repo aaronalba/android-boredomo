2feb3421f7332b29a8ff731c06a48a6b7d009e92
aaronalba <aaalba1@up.edu.ph>
2021-05-17 22:00:45 +0800

    ADD Utils class

---
+++ app/src/main/java/com/practice/boredomo/utils/Utils.java
@@ -0,0 +1,57 @@
+package com.practice.boredomo.utils;
+
+import com.practice.boredomo.R;
+
+/**
+ * Util Class for the application
+ * @author Aaron Alba
+ */
+public final class Utils {
+
+    /**
+     * Converts a given type chip id to the String name of the chip.
+     * @param id The id of the chip.
+     * @return The name of the chip.
+     */
+    public static String typeChipIdConverter(int id) {
+        if (id == R.id.education_chip) {
+            return "education";
+        } else if (id == R.id.recreational_chip) {
+            return "recreational";
+        } else if (id == R.id.social_chip) {
+            return "social";
+        } else if (id == R.id.diy_chip) {
+            return "diy";
+        } else if (id == R.id.charity_chip) {
+            return "charity";
+        } else if (id == R.id.cooking_chip) {
+            return "cooking";
+        } else if (id == R.id.relaxation_chip) {
+            return "relaxation";
+        } else if (id == R.id.music_chip) {
+            return "music";
+        } else if (id == R.id.busywork_chip) {
+            return "busywork";
+        } else {
+            return "";
+        }
+    }
+
+
+    /**
+     * Converts a given cost chip id to the String name of the chip.
+     * @param id chip id.
+     * @return the name of the chip.
+     */
+    public static String costChipIdConverter(int id) {
+        if (id == R.id.free_chip) {
+            return "free";
+        } else if (id == R.id.cheap_chip) {
+            return "cheap";
+        } else if (id == R.id.expensive_chip) {
+            return "expensive";
+        } else {
+            return "";
+        }
+    }
+}
