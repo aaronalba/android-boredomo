9da2c798ffe7493c1db2d8f1e7ddfe38c2af4dd1
aaronalba <aaalba1@up.edu.ph>
2021-05-17 13:31:26 +0800

    ADD listeners for the widgets in SearchFragment
    The function that will be called when the search button is clicked is
    now created but the implementation is not yet finished.

--- app/src/main/java/com/practice/boredomo/fragment/SearchFragment.java
+++ app/src/main/java/com/practice/boredomo/fragment/SearchFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,24 +24,54 @@ import com.practice.boredomo.R;
 public class SearchFragment extends Fragment {
     private TextView mParticipantValue;
     private Slider mParticipantSlider;
+    private ProgressBar mProgressBar;
+    private Button mSearchBtn;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_result, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
 
-//        // set listener to the slider
-//        mParticipantSlider = view.findViewById(R.id.search_participant_slider);
-//        mParticipantValue = view.findViewById(R.id.search_participant_value);
-//        mParticipantSlider.addOnChangeListener(new Slider.OnChangeListener() {
-//            @Override
-//            public void onValueChange(@NonNull Slider slider, float value, boolean fromUser) {
-//                String text = ((int)value)+"";
-//                mParticipantValue.setText(text);
-//            }
-//        });
+        // get references to the widgets
+        mParticipantSlider = view.findViewById(R.id.search_participant_slider);
+        mParticipantValue = view.findViewById(R.id.search_participant_value);
+        mProgressBar = view.findViewById(R.id.search_progressBar);
+        mSearchBtn = view.findViewById(R.id.search_button);
 
+
+        // set listener to the slider
+        mParticipantSlider.addOnChangeListener(new Slider.OnChangeListener() {
+            @Override
+            public void onValueChange(@NonNull Slider slider, float value, boolean fromUser) {
+                String text = ((int)value)+"";
+                mParticipantValue.setText(text);
+            }
+        });
+
+
+        // set onClick to the search button
+        mSearchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                search(v);
+            }
+        });
 
         return view;
+    }
+
+
+    /**
+     * Searches the API with the given paramters
+     * @param v The Button that was clicked
+     */
+    public void search(View v) {
+        // show the loading progress bar
+        mProgressBar.setVisibility(View.VISIBLE);
+
+        // compile the data to be sent to the api
+
+        // send the http request to the api
+
     }
 }
